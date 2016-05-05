c004d24e6cea080625ae628801f1a93d43a77ecf
janmaru <janmaru@gmail.com>
2016-05-05 23:37:32 +0200

    tutorial sql

---
+++ Tutorial1/SQLServer_Tutorial.fsx
@@ -0,0 +1,66 @@
+﻿#r @"..\SQLAccess\bin\Debug\SQLAccess.dll"
+
+open System
+open System.IO
+open System.Data
+open System.Data.SqlClient
+open SQLAccess
+open SQLAccess.Railway
+open SQLAccess.MappingData
+
+
+//BEGIN DAPPER -----------------------------------------------
+[<Literal>]
+let CN_STRING = @"Server=.\SQLEXPRESS;Database=test;Trusted_Connection=True;"
+     
+//the F# compiler emits a default constructor and property setters into the generated IL 
+//for this type (though those features are not exposed to F# code).  
+[<CLIMutable>] 
+type User = { Id:int ; Name:string; Surname:string; Age:int}
+
+let listOfUser =       
+    uQuery<User> (SQLServer, CN_STRING ,"SELECT * From tbUsers",null)
+
+listOfUser |> Seq.iter (fun x  ->  printfn "%s %s" x.Name x.Surname) 
+
+[<CLIMutable>] 
+type UserSelectArgs = { SelectedUserId:int}
+
+let getUser userId =
+    uQuery<User>  (SQLServer, CN_STRING ,"SELECT ID, Surname From tbUsers WHERE ID = @SelectedUserId",{SelectedUserId=userId})
+    |> Seq.head
+
+let singleUser = getUser 1  
+printfn "%s" singleUser.Surname
+// END DAPPER -----------------------------------------------
+
+
+//BEGIN SQLACCESS -------------------------------------------
+
+//UserId is an int NOT an int64 -->SQLServer
+type User2 = { UserId:int ; Name:string; Surname:string}
+
+let display user =
+        printfn "%A - %s %s" user.UserId  user.Name  user.Surname
+
+let toUser (reader: IDataReader) =
+    { 
+        UserId = unbox(reader.["ID"])
+        Name = unbox(reader.["Name"])
+        Surname = unbox(reader.["SurName"])
+    }  
+
+ 
+//railway pattern query without parameters
+let result= SQLData.query (SQLServer, 
+                           CN_STRING,
+                           "Select * from tbUsers", CommandType.Text,
+                           Seq.empty, 
+                           toUser)
+match result with
+| Success x -> Seq.iter display x
+| Failure y ->  printfn "errore: %s" y
+
+
+//END SQLACCESS   -------------------------------------------
+
